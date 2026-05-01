95ae884eee5d8ab1a25c9f79794040e74a20d652
tasneemmagdy <tasneemmagdy299@gmail.com>
2026-05-01 22:17:23 +0300

    add vercel flutter config

---
+++ install_flutter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:`pwd`/flutter/bin"
+flutter/bin/flutter config --enable-web
+flutter/bin/flutter pub get
